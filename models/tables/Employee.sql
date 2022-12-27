a80cf65c87a1398a1c0769d0a8ba138af4dcc3fb
harshFT <hp7728727@gmail.com>
2022-12-27 13:03:52 +0530

    added tables

---
+++ models/tables/Employee.sql
@@ -0,0 +1,19 @@
+-- rename the office to office id and genrate total working days of employee 
+
+with emp as (
+select 
+EmployeeID,
+Extension,
+EmployeeName,
+EmployeeGender,
+HIRE_DATE,
+datediff(day,HIRE_DATE,CURRENT_DATE()) as "WORKING DAYS",
+Office as OFFICE_ID,
+Supervisor,
+JobTitle,
+AnnualSalary,
+Sales_Target
+from PC_FIVETRAN_DB.SQL_SERVER_DBO.employees
+)
+
+select * from emp
